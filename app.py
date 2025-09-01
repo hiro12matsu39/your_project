ff8a74e5ca08ec1ce0bbef581d06c4fa894fec73
matsumoto hiroshi <hiro12.matsu39@gmail.com>
2025-09-01 21:41:17 +0900

    おはように変更

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html", message="こんにちは、Flask！")
+    return render_template("index.html", message="おはよう、Flask！")
 
 if __name__ == "__main__":
     app.run(port=5001, debug=True)
